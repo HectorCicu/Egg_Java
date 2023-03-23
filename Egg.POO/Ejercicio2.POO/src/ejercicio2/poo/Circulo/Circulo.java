5232aba6713ccd7ed4327857bf81e49409ebc33b
Hector Cicutti <hector.cicutti@gmail.com>
2023-03-23 18:32:06 -0300

    V1.0.3

---
+++ Egg.POO/Ejercicio2.POO/src/ejercicio2/poo/Circulo/Circulo.java
@@ -0,0 +1,27 @@
+
+
+package ejercicio2.poo.Circulo;
+
+/**
+ *
+ * @author Temporal
+ */
+public class Circulo {
+public double radio;
+
+    public Circulo() {
+    }
+
+    public Circulo(double radio) {
+        this.radio = radio;
+    }
+
+    public double getRadio() {
+        return radio;
+    }
+
+    public void setRadio(double radio) {
+        this.radio = radio;
+    }
+
+}
---
+++ Egg.POO/Ejercicio2.POO/src/ejercicio2/poo/CirculoMain.java
@@ -0,0 +1,29 @@
+
+package ejercicio2.poo;
+
+import ejercicio2.poo.Circulo.Circulo;
+
+/**
+ *
+ * @author hhc
+ */
+public class CirculoMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      
+        Circulo c1 = new Circulo();
+
+        Circulo c2 = new Circulo();
+
+        Circulo c3 = new Circulo();
+
+        Circulo c4 = new Circulo();
+
+        Circulo c5 = new Circulo();
+
+    }
+
+}
